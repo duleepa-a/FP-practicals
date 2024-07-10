3d8193e35212d5281a33fb606bb574c43144772f
duleepa-a <128412826+duleepa-a@users.noreply.github.com>
2024-07-10 23:33:40 +0530

    Add files via upload

---
+++ practical04.scala
@@ -0,0 +1,60 @@
+import scala.io.StdIn._
+object practical04{
+    var nameArray = Array("Pen","Pencil","Bottle")
+    var intArray = Array(10,20,30)
+
+    def displayInventory(): Unit ={
+        println("The inventory: ")
+        for(i <- 0 to nameArray.length - 1 ){
+            printf("%s : %d\n",nameArray(i),intArray(i))
+        }
+    }
+
+    def restockItem(name : String , quantity : Int) : Unit ={
+        if(nameArray.contains(name)){
+            var index = nameArray.indexOf(name)
+            intArray(index) = intArray(index) + quantity
+        }
+        else{
+            println("The entered item is not in the invnetory")
+        }
+    }
+
+    def sellItem(name : String, quantity : Int) : Unit = {
+        if(nameArray.contains(name)){
+            var index = nameArray.indexOf(name)
+            intArray(index) = intArray(index) - quantity
+        }
+        else{
+            println("The entered item is not in the invnetory")
+        }
+    }
+
+    def checkNum(num : Int) : Unit = num  match {
+        case  x if x <= 0 => println("Negative/Zero")
+        case x if x%2 == 1 => println("Odd Number is given")
+        case _ => println("Even Number is given")
+    }
+
+    def main(args: Array[String]): Unit = {
+        
+        //Question 1 .1)
+        printf("Question 01 :\n\n 1)Prining the inventory :")
+        displayInventory()
+
+        //Question 1 .2)
+        restockItem("Bottle",50)
+        printf("\n2)Adding 50 bottles to the stock: \nPrining the inventory :")
+        displayInventory()
+
+        //Question 1 .3)
+        sellItem("Pen",5)
+        printf("\n3)Selling 5 pens : \nPrining the inventory :")
+        displayInventory()
+
+        //Question 2
+        printf("Question 02 :\n Enter an integer:")        
+        val num = readInt()
+        checkNum(num)
+    }
+}
